f84fd16a5bc0baaa0d05cf0b0f6eeed9b1a0e4fb
Sawyer Redpath <sawyerbruceredpath@gmail.com>
2022-03-02 10:47:18 -0800

    6.1, 6.2

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import reducer from './reducer'
+
+const store = createStore(reducer)
+
+const App = () => {
+  const incrementGood = () => {
+    store.dispatch({
+      type: 'GOOD'
+    })
+  }
+
+  const incrementOk = () => {
+    store.dispatch({
+      type: 'OK'
+    })
+  }
+
+  const incrementBad = () => {
+    store.dispatch({
+      type: 'BAD'
+    })
+  }
+
+  const reset = () => {
+    store.dispatch({
+      type: 'ZERO'
+    })
+  }
+
+  return (
+    <div>
+      <button onClick={incrementGood}>good</button>
+      <button onClick={incrementOk}>ok</button>
+      <button onClick={incrementBad}>bad</button>
+      <button onClick={reset}>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+    </div>
+  )
+}
+
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root'))
+}
+
+renderApp()
+store.subscribe(renderApp)
